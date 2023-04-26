2c245d9fb8d9ff742515e4fe92e8cde9d35edde6
Thatiana Sophia <thatysophia08@gmail.com>
2023-04-25 20:48:00 -0400

    Controller para receber as requisições de pesquisa de produto.

---
+++ Buscador/Controllers/ProdutosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Buscador.Models;
+
+namespace Buscador.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ActionResult Index(string categoria, string pesquisa, string loja = "todas")
+        {
+            List<Produto> produtosMercadoLivre = new List<Produto>();
+            List<Produto> produtosBuscape = new List<Produto>();
+
+            if (loja == "mercado-livre" || loja == "todas")
+            {
+                produtosMercadoLivre = MercadoLivre.BuscarProdutos(categoria, pesquisa);
+            }
+
+            if (loja == "buscape" || loja == "todas")
+            {
+                produtosBuscape = Buscape.BuscarProdutos(categoria, pesquisa);
+            }
+
+            List<Produto> produtosCombinados = produtosMercadoLivre.Concat(produtosBuscape).ToList();
+
+            ViewBag.Categoria = categoria;
+            ViewBag.ProdutosCombinados = produtosCombinados;
+
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
